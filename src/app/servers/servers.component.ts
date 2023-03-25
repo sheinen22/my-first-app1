1a045fe18b20e6ebe5a1014e0b91efb35695124c
Stephen Heinen <stephenheinen22@yahoo.com>
2023-03-25 15:34:54 -0500

    playing with different binding options

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,9 +2,30 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  template: '<app-server></app-server><app-server></app-server>',
+  // template: '<app-server></app-server><app-server></app-server>',
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent {
+  allowNewServer = false;
+  serverCreationStatus = "No server was created";
+  serverName = '';
 
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000)
+  }
+
+  ngOnInit() {
+
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created!'
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
 }
