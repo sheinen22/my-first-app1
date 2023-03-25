ad89472d0658ffb5d6a5cd737e1a80afe9040577
Stephen Heinen <stephenheinen22@yahoo.com>
2023-03-25 18:13:49 -0500

    messing with Directives, structual - ngIf, ngFor.  attribute - ngStyle, ngClass

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -11,6 +11,7 @@ export class ServersComponent {
   serverCreationStatus = "No server was created";
   serverName = 'Testserver';
   serverCreated = false;
+  servers = ['Testserver', 'Testserver 2'];
 
   constructor() {
     setTimeout(() => {
@@ -24,6 +25,7 @@ export class ServersComponent {
 
   onCreateServer() {
     this.serverCreated = true
+    this.servers.push(this.serverName)
     this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
   }
 
